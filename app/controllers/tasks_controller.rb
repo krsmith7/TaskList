94c9c5acb7836103f826f486cb2acbe2e32e2b86
Katricia Smith <smithkatricia@gmail.com>
2018-09-18 15:40:51 -0700

    Delete hard coded task list. Edit show and index methods for new Task instances. Add 4 Tasks.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,10 @@
-TASKS = [
-  "Test VR intro video",
-  "Email machine learning group",
-  "Purchase ticket to Brazil",
-  "Finish Unit 7 of Portuguese Lessons",
-  "Purchase monitors for Rio team"
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    return @tasks = Task.all
   end
 
   def show
-    task_id = params[:id].to_i
-    @task = TASKS[task_id]
+    task_id = params[:id]
+    @task = Task.find(task_id)
   end
 end
