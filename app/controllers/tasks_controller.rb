11850561794dbae9f66caabae8ba38e2dc78b2ae
Katricia Smith <smithkatricia@gmail.com>
2018-09-20 10:07:36 -0700

    Change fnew view form to form_with model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,8 +15,9 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(action: params[:task][:action], description: params[:task][:description], completion_date: params[:task][:completion_date])
 
+
     if @task.save
-      redirect_to tasks_path
+      redirect_to root_path
     else
       render :new
     end
