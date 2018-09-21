3b067f6252f4bbe4af01c46d698cbadc4e1cfca1
Katricia Smith <smithkatricia@gmail.com>
2018-09-21 10:08:26 -0700

    Edit update action. Add destroy action.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,14 +3,17 @@ class TasksController < ApplicationController
     return @tasks = Task.all
   end
 
+
   def show
     task_id = params[:id]
     @task = Task.find(task_id)
   end
 
+
   def new
     @task = Task.new
   end
+
 
   def create
     @task = Task.new(action: params[:task][:action], description: params[:task][:description], completion_date: params[:task][:completion_date])
@@ -22,18 +25,28 @@ class TasksController < ApplicationController
     end
   end
 
+
   def edit
     @task = Task.find(params[:id])
   end
 
+
   def update
     @task = Task.find(params[:id])
 
-    if @task.update
-      redirect_to task_path
+    if @task.update(action: params[:task][:action], description: params[:task][:description], completion_date: params[:task][:completion_date])
+
+        redirect_to task_path
     else
       render :edit
     end
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+
+    task.destroy
+    redirect_to tasks_path
   end
 
 end
