f4bca82fe0a9b2c976e62a468103077209aea42a
Katricia Smith <smithkatricia@gmail.com>
2018-09-22 12:51:58 -0700

    Include column for is_complete in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_183413) do
+ActiveRecord::Schema.define(version: 2018_09_22_194701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_09_18_183413) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "completion_date"
+    t.integer "is_complete"
   end
 
 end
